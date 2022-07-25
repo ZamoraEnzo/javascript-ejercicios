4c65aa52642c3f86f14124f6b8e99dee54ae9247
ZamoraEnzo <enzozamoraa99@gmail.com>
2022-07-25 15:31:12 -0300

    mensaje

--- js.js
+++ js.js
@@ -1,59 +1,59 @@
-{function igualdad(num1, num2){
-if (num1==num2){
-    return "los valores son iguales"
-}
+// {function igualdad(num1, num2){
+// if (num1==num2){
+//     return "los valores son iguales"
+// }
 
-return "los valores no son iguales"
+// return "los valores no son iguales"
 
-}
-let comparo= igualdad(12,12)
-console.log (comparo)}
+// }
+// let comparo= igualdad(12,12)
+// console.log (comparo)}
 
-function mismalongitud(str1,str2){
- if (str1==str2){
- return true
- }
- return false
- }
- let enzo = mismalongitud(50,50)
- console.log(enzo)
+// function mismalongitud(str1,str2){
+//  if (str1==str2){
+//  return true
+//  }
+//  return false
+//  }
+//  let enzo = mismalongitud(50,50)
+//  console.log(enzo)
 
-function funcion (str1,str2){
-if (str1.length == str2.length){
-return "son identicas"
-}
-return "no son identicas"
-}
-let enzo=funcion(10,10)
-console.log(enzo)
-function elevaralcuadrado(num){
-return num**3
-}
-enzo = elevaralcuadrado(9)
-console.log(enzo)
+// function funcion (str1,str2){
+// if (str1.length == str2.length){
+// return "son identicas"
+// }
+// return "no son identicas"
+// }
+// let enzo=funcion(10,10)
+// console.log(enzo)
+// function elevaralcuadrado(num){
+// return num**3
+// }
+// enzo = elevaralcuadrado(9)
+// console.log(enzo)
 
-function numerorandom(num){
-if 
-(num==0)return "el numero es falso" 
+// function numerorandom(num){
+// if 
+// (num==0)return "el numero es falso" 
 
-else if
-(num<0) return "el numero es negativo"
+// else if
+// (num<0) return "el numero es negativo"
 
-else if 
-(num>0) return "el numero es positivo"
-}
-enzo=numerorandom(0)
-console.log(enzo)
+// else if 
+// (num>0) return "el numero es positivo"
+// }
+// enzo=numerorandom(0)
+// console.log(enzo)
 
-function euroadolar(num){
-    return num*1.20
-}
-let cambio=euroadolar(50)
-console.log(enzo)
-function esvocal (letra){
-if (letra == "a"|| letra=="e"||letra=="i"||letra =="o"||letra=="u")
-return "es vocal"
-} 
-let resultado= esvocal("c")
-console.log(resultado)
+// function euroadolar(num){
+//     return num*1.20
+// }
+// let cambio=euroadolar(50)
+// console.log(enzo)
+// function esvocal (letra){
+// if (letra == "a"|| letra=="e"||letra=="i"||letra =="o"||letra=="u")
+// return "es vocal"
+// } 
+// let resultado= esvocal("c")
+// console.log(resultado)
 
