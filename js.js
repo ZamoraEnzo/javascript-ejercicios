d3be123744df168ad23de37933ee563fd87e70c6
ZamoraEnzo <enzozamoraa99@gmail.com>
2022-07-26 23:26:23 -0300

    mensaje

--- js.js
+++ js.js
@@ -1,59 +1,100 @@
-{function igualdad(num1, num2){
-if (num1==num2){
-    return "los valores son iguales"
-}
+// {function igualdad(num1, num2){
+// if (num1==num2){
+//     return "los valores son iguales"
+// }
 
-return "los valores no son iguales"
+// return "los valores no son iguales"
 
-}
-let comparo= igualdad(12,12)
-console.log (comparo)}
+// }
+// let comparo= igualdad(12,12)
+// console.log (comparo)}
 
-function mismalongitud(str1,str2){
- if (str1==str2){
- return true
- }
- return false
- }
- let enzo = mismalongitud(50,50)
- console.log(enzo)
+// function mismalongitud(str1,str2){
+//  if (str1==str2){
+//  return true
+//  }
+//  return false
+//  }
+//  let enzo = mismalongitud(50,50)
+//  console.log(enzo)
 
-function funcion (str1,str2){
-if (str1.length == str2.length){
-return "son identicas"
-}
-return "no son identicas"
-}
-let variable=funcion(10,10)
-console.log(enzo)
-function elevaralcuadrado(num){
-return num**3
-}
-enzo = elevaralcuadrado(9)
-console.log(enzo)
+// function funcion (str1,str2){
+// if (str1.length == str2.length){
+// return "son identicas"
+// }
+// return "no son identicas"
+// }
+// let variable=funcion(10,10)
+// console.log(enzo)
+// function elevaralcuadrado(num){
+// return num**3
+// }
+// enzo = elevaralcuadrado(9)
+// console.log(enzo)
 
-function numerorandom(num){
-if 
-(num==0)return "el numero es falso" 
+// function numerorandom(num){
+// if 
+// (num==0)return "el numero es falso" 
 
-else if
-(num<0) return "el numero es negativo"
+// else if
+// (num<0) return "el numero es negativo"
 
-else if 
-(num>0) return "el numero es positivo"
-}
-enzo=numerorandom(0)
-console.log(enzo)
+// else if 
+// (num>0) return "el numero es positivo"
+// }
+// enzo=numerorandom(0)
+// console.log(enzo)
 
-function euroadolar(num){
-    return num*1.20
-}
-let cambio=euroadolar(50)
-console.log(enzo)
-function esvocal (letra){
-if (letra == "a"|| letra=="e"||letra=="i"||letra =="o"||letra=="u")
-return "es vocal"
-} 
-let resultado= esvocal("c")
-console.log(resultado)
+// function euroadolar(num){
+//     return num*1.20
+// }
+// let cambio=euroadolar(50)
+// console.log(enzo)
+// function esvocal (letra){
+// if (letra == "a"|| letra=="e"||letra=="i"||letra =="o"||letra=="u"){
+// return "es vocal"}
+// return "no es una vocal"
+// }
+// console.log(esvocal("c"))
+
+// function igualdad(num){
+//     if (num>=10 && num<=12){
+//      return "es el numero 10,11,12"   
+//     }
+// return "no es el numero 10,11,12"
+// }
+// console.log(igualdad(10))
+
+// function letra (char) {
+//     switch (char){
+//     case "a" :
+//     console.log ("A")
+//     break
+//       case "b" :
+//     console.log("B")  
+//     break
+//     default: console.log("error")
+// }
+// }
+// console.log(letra("c"))
+//   1=river
+//   2=boca
+//   3=patronato
+//   4=chacarita
+  
+// let myArray = [15,4848,54 ,"enzo", "false" ]
+// let cat={
+// "name" : "pepe",
+// "legs": 4,
+// "tails":1,
+// "enemies":["water","dogs"]
+// } 
+// let numero=myArray[3]
+// console.log(numero)
+// let nameCat=cat.legs
+// console.log(nameCat)
+// let perros=cat.enemies[1]
+// console.log(perros)
+
+
 
